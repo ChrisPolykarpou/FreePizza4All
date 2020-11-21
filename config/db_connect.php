f3662bdcc7ac01401930c8b7c323921d68c4d3ff
christofer <christofer-polykarpou@hotmail.com>
2020-11-21 22:52:08 +0200

    ok

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,6 +1,6 @@
 <?php
   //connect to mySQL
-  $conn = mysqli_connect('localhost', 'chris', '12345', 'mixPizza');
+  $conn = mysqli_connect('localhost', 'root', 'CHr16091997', 'mixPizza');
 
   if(!$conn){
     echo "Connection Error" . mysqli_connect_error();
