18b516a9d7f80fa043ccada53be4c1b078f635b4
christofer <christofer-polykarpou@hotmail.com>
2020-11-22 15:35:23 +0200

    ok

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ mysqli_close($conn);
     align-items: center;
     flex-flow: row;
     height: 310px;
+    width: 635px;
   }
 
   .slidis {
@@ -39,10 +40,7 @@ mysqli_close($conn);
   .bite {
     padding-top: 25px;
   }
-
-  .md3{
-    width: 635px;
-  }
+  
 </style>
 
 <!--Import jQuery before materialize.js-->
